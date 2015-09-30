125c9082df9e6f1c78e52139c95fe17a63b40cd3
Chyngyz Dyikanbaev <chipalino@live.com>
2015-09-30 16:05:09 +0600

    Update relvis.js

--- relvis.js
+++ relvis.js
@@ -1,25 +1,27 @@
-var RelVisJs = (function(jQuery) {
+(function (w,q) {
+  w.RelVisJs = (function(q) {
 
-  function RelVisJs(path, height, width) {
-    this.height = height != null ? height : 400;
-    this.width = width != null ? width : 400;
-    this.path = path+"_svg";
-    this.conns = {};
-    jQuery('#'+path).html('<svg id="'+this.path+'" height="'+this.height+'" width="'+this.width+'"><svg>');
+    function RelVisJs(path, height, width) {
+      this.height = height != null ? height : 400;
+      this.width = width != null ? width : 400;
+      this.path = path+"_svg";
+      this.conns = {};
+      q('#'+path).html('<svg id="'+this.path+'" height="'+this.height+'" width="'+this.width+'"><svg>');
+    }
+    
+    return RelVisJs;
+
+  })(q);
+
+  RelVisJs.prototype.addNode = function(name, title) {
+    title = title != null ? title : name;
+    this.conns[name] = {
+      title:title,
+      conns: []
+    }
   }
-  
-  return RelVisJs;
 
-})(jQuery);
-
-RelVisJs.prototype.addNode = function(name, title) {
-  title = title != null ? title : name;
-  this.conns[name] = {
-    title:title,
-    conns: []
+  RelVisJs.prototype.addPath = function(a, b) {
+    this.conns[a]['conns'].push(b);
   }
-}
-
-RelVisJs.prototype.addPath = function(a, b) {
-  this.conns[a]['conns'].push(b);
-}
+})(window, jQuery);
